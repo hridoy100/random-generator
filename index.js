3e3c503e00bfbc90dc94f83e1b23ba1654637410
Raihanul Alam Hridoy <raihanul.hridoy@revesoft.com>
2022-04-12 00:55:45 +0600

    gitignore file added

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+
+app.get('/', (req, res) => {
+	res.send('Welcome!')
+})
+
+app.listen(port, ()=> {
+	console.log(`Server is listening on port ${port}..`)
+})
