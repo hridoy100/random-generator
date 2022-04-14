f41b5b6326fad6603ba1ff095e664cb872a456cf
Raihanul Alam Hridoy <raihanul.hridoy@revesoft.com>
2022-04-15 04:52:06 +0600

    generate function improvement: instead of using fs.appendFileSync(), used   var out = fs.createWriteStream(fileName, { flags : 'a' }); and out.write.   This improved the function call and connection reset for 2 MB files.

--- index.js
+++ index.js
@@ -13,23 +13,23 @@ ASCII values: a-z = 97 - 97+25
 */
 
 function randomInt(len){
-  console.log(len);
+  // console.log(len);
   var intVal = 0, i=0, min=0, max=10;
   var sign = (Math.random()*10<<0)%2===0?1:-1;
   let intLen = len==0?(Math.random() * (max - min) + min << 0):len;
   intVal=Math.random()*Math.pow(10, intLen) << 0;
   intVal*=sign;
-  console.log(" "+intVal.toString().length);
+  // console.log(" "+intVal.toString().length);
   return intVal;
 }
 
 function randomRealNum(len){
-  console.log(len);
+  // console.log(len);
   len = len==0?(Math.random()*10)<<0:len-2;
   var val = Math.random()*Math.pow(10, len);
   var fixedDecimalPoint = (Math.random()*5)<<0;
   val = val.toFixed(len==0?fixedDecimalPoint:2);
-  console.log(" "+val.toString().length);
+  // console.log(" "+val.toString().length);
   return val;
 }
 
@@ -47,7 +47,7 @@ function getRandomInt(){
 
 function randomString(an, len){
   var str = "", i=0, min=5, max=20;
-  console.log(len);
+  // console.log(len);
   let strLen = len==0?(Math.random() * (max - min) + min << 0) : len;
   let charVal, numVal;
   for(; i++<strLen; ){
@@ -68,49 +68,57 @@ function randomString(an, len){
 
 }
 
-function generate(len){
-  var str, num, data="", curLen=0;
-  console.log(len);
-  if(len!=0) {
+function generate(len) {
+  var str, num, data = "", curLen = 0;
+  // console.log("len: " +len);
+  if (len != 0) {
     curLen = (len / 4) << 0;
-    curLen-=2;
+    curLen -= 2;
     console.log("curLen: " + curLen);
   }
 
+  // console.log("len: " +len);
+
   str = randomString(" ", curLen);
-  data+=str;
-  data+=", "
+  data += str;
+  data += ", "
   // console.log("str: " + str + " len="+str.length);
   num = randomRealNum(curLen);
-  data+=num;
-  data+=", "
+  data += num;
+  data += ", "
   // console.log("Rnum: " + num + " len="+num.length);
   num = randomInt(curLen);
-  data+=num;
+  data += num;
 
-  data+=", "
+  data += ", "
   // console.log("num: " + num + " len="+num.toString().length);
-  if(len==0)
+  if (len === 0)
     str = randomString("an", len);
-  else
-    str = randomString("an", len-data.length-2);
+  else {
+    str = randomString("an", len - data.length);
+  }
 
   data+=str;
+  if(len===0) {
+    data += ", ";
+  }
   // console.log("data: " + data);
-  data+=", ";
   return data;
 }
 
 
 function generate2MBData(fileName){
-  var maxChars = 2048, calc=0;
+  var maxChars = 2048000, calc=0;
   var data ="";
   fs.writeFile(fileName, "", (err) => {
     if(err) throw err;
   });
+  var out = fs.createWriteStream(fileName, { flags : 'a' });
   while (calc<maxChars) {
-    data = generate(0);
+    // console.log(calc);
     if((maxChars-calc)<20*2+10*2+6+10 && (maxChars-calc)>11){
+      // console.log("inside: generate with len");
+      // console.log((maxChars-calc));
       data = generate(maxChars-calc);
     }
     else if ((maxChars-calc)<=11){
@@ -118,11 +126,18 @@ function generate2MBData(fileName){
       calc=maxChars;
       continue;
     }
+    else {
+      data = generate(0);
+    }
     // console.log("len: " + data.length);
-    fs.appendFileSync(fileName, data,(err) => {
+    /*fs.appendFileSync(fileName, data,(err) => {
       // In case of a error throw err.
-      if (err) throw err;
-    })
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+    })*/
+    out.write(data, 'utf-8');
     // fs.close("output.txt", (err) => {
     //   if (err)
     //     console.error('Failed to close file', err);
@@ -132,13 +147,14 @@ function generate2MBData(fileName){
     // });
     calc+=data.length;
   }
+  out.end();
 }
 
 function calculateObjects(fileName){
   var data="";
   try {
     data = fs.readFileSync(fileName, 'utf8')
-    console.log(data)
+    // console.log(data)
   } catch (err) {
     console.error(err)
   }
@@ -165,27 +181,47 @@ function calculateObjects(fileName){
       intCount++;
     }
   }
-  console.log("alphaCount="+alphaCount);
-  console.log("strCount="+strCount);
-  console.log("realCount="+realCount);
-  console.log("intCount="+intCount);
+  // console.log("alphaCount="+alphaCount);
+  // console.log("strCount="+strCount);
+  // console.log("realCount="+realCount);
+  // console.log("intCount="+intCount);
+  var obj = {
+    alpha: alphaCount,
+    str: strCount,
+    int: intCount,
+    real: realCount
+  }
+
+  return obj;
 }
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
 
+
+
 app.get('/download', (req, res) => {
-  res.sendFile(__dirname+"/output.txt");
+  res.download(__dirname+"/output.txt");
 });
 
 app.get('/report', (req, res) => {
-  calculateObjects(__dirname + "/output.txt");
-  res.send("done");
+  var obj = calculateObjects(__dirname + "/output.txt");
+  res.send(JSON.stringify(obj));
 });
 
-app.listen(port, ()=> {
+app.get('/generate', (req, res) => {
+  // console.log("generate called");
   var fileName = "output.txt";
   generate2MBData(fileName);
+  var obj={
+    fileName: "output.txt",
+    url: "http://localhost:3000/download",
+  }
+  // console.log("generation ended");
+  res.send(JSON.stringify(obj));
+})
+
+app.listen(port, ()=> {
   console.log(`Server is listening on port ${port}..`)
 })
